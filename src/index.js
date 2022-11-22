118e8280051e41d9004fd8313282efd9eb17f926
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-11-22 17:59:36 +0100

    fix: Fix bills retrieving

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ const {
 const models = cozyClient.new.models
 const { Qualification } = models.document
 
-const moment = require('moment')
 const USER_AGENT =
   'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0 Cozycloud'
 const request = requestFactory({
@@ -41,21 +40,14 @@ async function start(fields) {
   log('info', 'Successfully logged in')
   log('info', 'Getting the session id')
   const $compte = await request('https://moncompte.mediapart.fr/')
-  const session = $compte('iframe[src*=moncompte]')
-    .first()
-    .attr('src')
-    .match(/sess=([^&]*)/)[1]
   log('info', 'Fetching the list of documents')
-  const $list = await request(
-    `https://moncompte.mediapart.fr/base/moncompte/ajax/index.php?abonnement=0&sess=${session}`
-  )
   log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($list)
-  log('debug', documents, 'docs')
+  const documents = await parseDocuments($compte)
   log('info', 'Saving data to Cozy')
   await saveBills(documents, fields.folderPath, {
     keys: ['vendor', 'billId'], // deduplication keys
     identifiers: ['mediapart'], // bank operations
+    fileIdAttributes: ['billId'],
     sourceAccount: this.accountId,
     sourceAccountIdentifier: fields.login,
     contentType: 'application/pdf'
@@ -82,141 +74,64 @@ function authenticate(name, password) {
   })
 }
 
-/* This function scrape differents tables found
-   About html:
-     - 2 kind of table are found, one with <li> contains recents bills (11€)
-     one with <tr> contains old bills (9€)
-     - When account is old and not use anymore, bills are concat in only a <tr> table
-     - When some gift card(for mediapart or courrier international is used, some line can
-     have no pdf and no price
-   Strategy :
-     We try to scrape about all <li> and all <tr> in all table found.
-     Most known errors was about <tr> being not a bill line scrapable (header, gift card)
-     Bad <tr> part are avoid by their missing 'valign: middle' or their missing pdf link
-*/
 function parseDocuments($) {
-  // Common constantes
-  const vendor = 'Mediapart'
-  const date = new Date()
-  const version = 1
-  const metadata = { date, version }
-
-  const listOfRecents = $('table li')
-    .filter(function (idx, li) {
-      return $(li).find('a').length !== 0 // Throw line without pdf link
-    })
-    .map(function (idx, li) {
-      try {
-        const re =
-          /.*(\d\d\/\d\d\/\d\d\d\d).*(\d\d\/\d\d\/\d\d\d\d)[\s.]*(\d+,\d\d)\s+(\S+).*/
-        const [, start, end, price, currency] = $(li).text().match(re)
-        const oStart = moment.utc(start, 'DD/MM/YYYY')
-        const oEnd = moment.utc(end, 'DD/MM/YYYY')
-        const startDate = oStart.format('YYYY-MM-DD')
-        const endDate = oEnd.format('YYYY-MM-DD')
-        const date = oEnd.toDate()
-        const href = $('a', li).first().attr('href')
-        const fileurl = `https://moncompte.mediapart.fr/base/moncompte/${href}`
-        const billId = href.match(/get_facture=([^&]+)/)[1]
-        const title = `Mediapart ${billId} ${startDate} - ${endDate}`
-        const filename = `mediapart_${billId}_${startDate}_${endDate}.pdf`
-        const amount = parseFloat(price.replace(',', '.'))
-        return {
-          title,
-          metadata,
-          date,
-          startDate,
-          endDate,
-          amount,
-          vendor,
-          billId,
-          currency,
-          filename,
-          fileurl,
-          fileAttributes: {
-            metadata: {
-              contentAuthor: 'mediapart.fr',
-              issueDate: moment.utc(new Date(), 'YYYY-MM-DD'),
-              datetime: startDate,
-              datetimeLabel: `issueDate`,
-              isSubscription: true,
-              carbonCopy: true,
-              qualification: Qualification.getByLabel('other_invoice')
-            }
-          },
-          requestOptions: {
-            headers: {
-              'User-Agent': USER_AGENT
-            }
-          }
+  const orders = []
+  $('.billing__list__item').each((i, el) => {
+    const billData = $(el).html().replace(/\s\s+/g, ' ')
+    const [foundHref, billId] = billData.match(
+      /<a href="\/facture\/(\d*)\/telecharger"/
+    )
+    const href = foundHref.split('"')[1]
+    const foundDates = billData.match(
+      /Du (\d){2}\/(\d){2}\/(\d){4} au (\d){2}\/(\d){2}\/(\d){4}/
+    )[0]
+    const [, foundStartDate, , foundEndDate] = foundDates.split(' ')
+    const { startDate, endDate } = formatDates(foundStartDate, foundEndDate)
+    const fileurl = `https://moncompte.mediapart.fr${href}`
+    const title = `Mediapart ${billId} ${startDate} - ${endDate}`
+    const filename = `mediapart_${billId}_${startDate}_${endDate}.pdf`
+    const [amount, currency] = billData
+      .match(/ ([0-9.]*) €/)[0]
+      .trim()
+      .split(' ')
+    orders.push({
+      vendor: 'Mediapart',
+      startDate,
+      endDate,
+      date: new Date(endDate),
+      fileurl,
+      billId,
+      title,
+      filename,
+      amount: parseFloat(amount),
+      currency,
+      fileAttributes: {
+        metadata: {
+          contentAuthor: 'mediapart.fr',
+          issueDate: new Date(),
+          datetime: new Date(startDate),
+          datetimeLabel: `issueDate`,
+          isSubscription: true,
+          carbonCopy: true,
+          qualification: Qualification.getByLabel('other_invoice')
         }
-      } catch (err) {
-        log('warn', 'Impossible to parse one line')
-        log('warn', JSON.stringify(err))
+      },
+      requestOptions: {
+        headers: {
+          'User-Agent': USER_AGENT
+        }
       }
     })
-    .get()
-  log('debug', `${listOfRecents.length} bills found in <li> style table`)
+  })
+  return orders
+}
 
-  let listOfOlds = []
-  listOfOlds = $('table tr')
-    .filter(function (idx, tr) {
-      // Throw lines not format like a bill line
-      // Throw lines with no pdf link as subcribe gift
-      return $(tr).attr('valign') === 'middle' && $(tr).find('a').length !== 0
-    })
-    .map(function (idx, tr) {
-      try {
-        const re = /.*(\d\d\/\d\d\/\d\d\d\d).*(\d\d\/\d\d\/\d\d\d\d).*/
-        const [, start, end] = $(tr).find('td').text().trim().match(re)
-        const oStart = moment.utc(start, 'DD/MM/YYYY')
-        const oEnd = moment.utc(end, 'DD/MM/YYYY')
-        const startDate = oStart.format('YYYY-MM-DD')
-        const endDate = oEnd.format('YYYY-MM-DD')
-        const date = oEnd.toDate()
-        const [price, currency] = $(tr)
-          .find('td')
-          .eq(1)
-          .text()
-          .trim()
-          .split('\xa0') // Unbreakable space
-        const href = $('a', tr).first().attr('href')
-        const fileurl = `https://moncompte.mediapart.fr/base/moncompte/${href}`
-        const billId = href.match(/get_facture=([^&]+)/)[1]
-        const title = `Mediapart ${billId} ${startDate} - ${endDate}`
-        const filename = `mediapart_${billId}_${startDate}_${endDate}.pdf`
-        const amount = parseFloat(price.replace(',', '.'))
-        return {
-          title,
-          metadata,
-          date,
-          startDate,
-          endDate,
-          amount,
-          vendor,
-          billId,
-          currency,
-          filename,
-          fileurl,
-          fileAttributes: {
-            metadata: {
-              contentAuthor: 'mediapart.fr',
-              issueDate: moment.utc(new Date(), 'YYYY-MM-DD'),
-              datetime: date,
-              datetimeLabel: `issueDate`,
-              isSubscription: true,
-              carbonCopy: true,
-              qualification: Qualification.getByLabel('other_invoice')
-            }
-          }
-        }
-      } catch (err) {
-        log('warn', 'Impossible to parse one line')
-        log('warn', JSON.stringify(err))
-      }
-    })
-    .get()
-  log('debug', `${listOfOlds.length} bills found in <tr> style table`)
-
-  return listOfRecents.concat(listOfOlds)
+function formatDates(startDate, endDate) {
+  const [startDay, startMonth, startYear] = startDate.split('/')
+  const [endDay, endMonth, endYear] = endDate.split('/')
+  const formatedDates = {
+    startDate: `${startYear}-${startMonth}-${startDay}`,
+    endDate: `${endYear}-${endMonth}-${endDay}`
+  }
+  return formatedDates
 }
